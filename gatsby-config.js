aabb4e76796af11f9e59fd2d468974526be04ce7
Morgan Baker <morganbakerdesigner@gmail.com>
2019-09-20 22:25:19 +0100

    Search backend added

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const queries = require("./src/utils/algolia")
+require("dotenv").config()
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -17,6 +19,15 @@ module.exports = {
     githubURL: `https://github.com/bagseye`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries,
+        chunkSize: 10000,
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
